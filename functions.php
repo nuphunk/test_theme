91583367acb9e6d5e391244f945f7c994503d711
Peter Williams <peter@peter-williams.com>
2019-02-10 17:04:13 +0000

    Update functions.php
    Changed it back to the correct thing

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package Dev Theme - Astra Child
+ * @package West Norwood Feast - Astra Child
  * @since 1.0.0
  */
 
