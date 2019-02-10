6924c2e53cf9ff7a90a5adf781f2f62ca7b96ab5
Peter Williams <peter@peter-williams.com>
2019-02-10 17:00:50 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package West Norwood Feast - Astra Child
+ * @package Dev Theme - Astra Child
  * @since 1.0.0
  */
 
