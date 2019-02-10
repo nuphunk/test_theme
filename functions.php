daa4beaeda8f24db0606077de43e12e48eb35932
nuphunk <5713405+nuphunk@users.noreply.github.com>
2019-02-10 20:32:44 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package West Norwood Feast - Astra Child
+ * @package West Norwood Feast - Astra Childs
  * @since 1.0.0
  */
 
